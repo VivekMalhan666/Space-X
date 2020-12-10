2f6f488c6ab2e04c1be59906aa4d7911197c03b7
vivek666 <amalhan43@gmail.com>
2020-12-10 19:07:24 +0530

    setting up basic layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import "./utils/App.css";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header"></header>
+		<div className="app">
+			<div className="app__header">
+				<h2>SpaceX Launch Program</h2>
+			</div>
+			<div className="app__body">
+				{/* Filters */}
+				{/* Filtered Cards */}
+			</div>
 		</div>
 	);
 }
